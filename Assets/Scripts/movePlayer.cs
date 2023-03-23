68f2fe3648e041f2f8835e71e4505b34adf4bc21
aj-patalinghog <99686240+aj-patalinghog@users.noreply.github.com>
2023-03-23 12:13:01 -1000

    Initial commit

---
+++ Assets/Scripts/movePlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayer : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    public float walkingSpeed = 5f;
+    private Vector2 _walkingInput;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        animator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        _walkingInput.x = Input.GetAxisRaw("Horizontal") * walkingSpeed;
+        _walkingInput.y = Input.GetAxisRaw("Vertical") * walkingSpeed;
+    }
+
+    void FixedUpdate() {
+        rb.velocity = transform.TransformDirection(_walkingInput);
+    }
+}
